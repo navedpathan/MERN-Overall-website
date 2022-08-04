d2c0ffe76f5390b39bbef1c40fa31693b8f37caf
Naved Pathan <navedpathan646@gmail.com>
2022-08-04 13:04:24 +0530

    new commit

--- router/auth.js
+++ router/auth.js
@@ -40,129 +40,129 @@ const User = require("../models/userSchema");
 
 // Async-Await
 
-// router.post('/register', async (req, res) => {
+router.post('/register', async (req, res) => {
 
-//     const { name, email, phone, work, password, cpassword } = req.body;
+    const { name, email, phone, work, password, cpassword } = req.body;
 
-//     if (!name || !email || !phone || !work || !password || !cpassword) {
-//         return res.status(422).json({ error: "Please filled the field properly" })
-//     }
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "Please filled the field properly" })
+    }
 
-//     try {
+    try {
 
-//         const userExist = await User.findOne({ email: email });
+        const userExist = await User.findOne({ email: email });
 
-//         if (userExist) {
-//             return res.status(422).json({ error: "Email already Exist" });
-//         }
-//         else if (password != cpassword) {
-//             return res.status(422).json({ error: "Password is not matching" });
-//         }
-//         else {
-//             const user = new User({ name, email, phone, work, password, cpassword });
-//             // between this from userSchema
-//             await user.save();
+        if (userExist) {
+            return res.status(422).json({ error: "Email already Exist" });
+        }
+        else if (password != cpassword) {
+            return res.status(422).json({ error: "Password is not matching" });
+        }
+        else {
+            const user = new User({ name, email, phone, work, password, cpassword });
+            // between this from userSchema
+            await user.save();
 
-//             res.status(201).json({ message: "user registered successfully" });
-//         }
+            res.status(201).json({ message: "user registered successfully" });
+        }
 
-//     } catch (err) {
-//         console.log(err);
-//     }
+    } catch (err) {
+        console.log(err);
+    }
 
-// });
+});
 
-// // login route
+// login route
 
-// router.post('/signin', async (req, res) => {
+router.post('/signin', async (req, res) => {
 
-//     try {
-//         // let token;
-//         const { email, password } = req.body;
+    try {
+        // let token;
+        const { email, password } = req.body;
 
-//         if (!email || !password) {
-//             return res.status(400).json({ error: "please filled the data" })
-//         }
+        if (!email || !password) {
+            return res.status(400).json({ error: "please filled the data" })
+        }
 
-//         const userLogin = await User.findOne({ email: email });
+        const userLogin = await User.findOne({ email: email });
 
-//         //  console.log(userLogin);
+        //  console.log(userLogin);
 
-//         if (userLogin) {
-//             const isMatch = await bcrypt.compare(password, userLogin.password);
+        if (userLogin) {
+            const isMatch = await bcrypt.compare(password, userLogin.password);
 
-//             const token = await userLogin.generateAuthToken();
-//             console.log(token);
+            const token = await userLogin.generateAuthToken();
+            console.log(token);
 
-//             res.cookie("jwtoken", token, {
-//                 expires: new Date(Date.now() + 25892000000),
-//                 httpOnly: true
-//             });
+            res.cookie("jwtoken", token, {
+                expires: new Date(Date.now() + 25892000000),
+                httpOnly: true
+            });
 
-//             if (!isMatch) {
-//                 res.status(400).json({ error: "Invalid Credentials" });
-//             }
-//             else {
-//                 res.json({ message: "User Signin successfully" });
-//             }
-//         }
+            if (!isMatch) {
+                res.status(400).json({ error: "Invalid Credentials" });
+            }
+            else {
+                res.json({ message: "User Signin successfully" });
+            }
+        }
 
-//         else {
-//             res.status(400).json({ error: "Invalid Credentials" });
-//         }
+        else {
+            res.status(400).json({ error: "Invalid Credentials" });
+        }
 
-//     } catch (err) {
-//         console.log(err);
-//     }
-// });
+    } catch (err) {
+        console.log(err);
+    }
+});
 
-// // about us page
+// about us page
 
-// router.get('/about', authenticate, (req, res) => {
-//     console.log(`Hello my About`);
-//     res.send(req.rootUser);
-// })
+router.get('/about', authenticate, (req, res) => {
+    console.log(`Hello my About`);
+    res.send(req.rootUser);
+})
 
-// // get user data for contact us & home page
+// get user data for contact us & home page
 
-// router.get('/getdata', authenticate, (req, res) => {
-//     console.log(`Hello my Homepage`);
-//     res.send(req.rootUser);
-// })
+router.get('/getdata', authenticate, (req, res) => {
+    console.log(`Hello my Homepage`);
+    res.send(req.rootUser);
+})
 
-// // Contact us page
+// Contact us page
 
-// router.post('/contact', authenticate, async (req, res) => {
-//     try {
-//         const { name, email, phone, message } = req.body;
+router.post('/contact', authenticate, async (req, res) => {
+    try {
+        const { name, email, phone, message } = req.body;
 
-//         if (!name || !email || !phone || !message) {
-//             console.log('error in contact form')
-//             return res.status(400).json({ error: "please filled the contact form" });
-//         }
+        if (!name || !email || !phone || !message) {
+            console.log('error in contact form')
+            return res.status(400).json({ error: "please filled the contact form" });
+        }
 
-//         const userContact = await User.findOne({ _id: req.userID });
+        const userContact = await User.findOne({ _id: req.userID });
 
-//         if (userContact) {
+        if (userContact) {
 
-//             const userMessage = await userContact.addMessage(name, email, phone, message);
+            const userMessage = await userContact.addMessage(name, email, phone, message);
 
-//             await userContact.save();
+            await userContact.save();
 
-//             res.status(201).json({ message: "User Contact successfully" });
-//         }
-//     }
-//     catch (err) {
-//         console.log(err);
-//     }
-// });
+            res.status(201).json({ message: "User Contact successfully" });
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
 
-// // Logout page
+// Logout page
 
-// router.get('/logout', (req, res) => {
-//     console.log(`Hello my Logout Page`);
-//     res.clearCookie("jwtoken", { path: '/' });
-//     res.status(200).send('User Logout');
-// })
+router.get('/logout', (req, res) => {
+    console.log(`Hello my Logout Page`);
+    res.clearCookie("jwtoken", { path: '/' });
+    res.status(200).send('User Logout');
+})
 
 module.exports = router;
